1bb4dc51e8beb229b58445c7a18a00b6521ebbe6
Ryan Rauch <ryan.rauch@outlook.com>
2025-10-20 13:25:34 -0500

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from "./LandingPage";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "VTable.ai | Smart Reservations";
+  }, []);
+
   return <LandingPage />;
 }
